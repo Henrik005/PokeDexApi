87c450f4ffcf12a9ed22f6d1655a4e4712ff34b5
Henrik005 <henrik.mathias.brathen@gmail.com>
2025-05-23 14:48:26 +0200

    Removed create table, begaan work on capture

--- PokeAPI/index.js
+++ PokeAPI/index.js
@@ -53,22 +53,56 @@ app.post("/api/createUser", async (req, res) => {
     try {
 		  const hashedPASSWORD = await bcrypt.hash(PASSWORD, 10);
         await sql.query`INSERT INTO users (EMAIL, NAME, PASSWORD) VALUES (${EMAIL}, ${NAME}, ${hashedPASSWORD})`;
-        await sql.query(`
-    CREATE TABLE ${NAME}_pokemon (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    user_id INT,
-    name VARCHAR(100) NOT NULL,
-    type VARCHAR(100) NOT NULL,
-    locations VARCHAR(500) NULL,
-    FOREIGN KEY (user_id) REFERENCES users(ID)
-  );
-`);
-        res.status(201).send("User created");
-    } catch (error) {
+        if(!NAME || !EMAIL){
+            return res.status(400).send(error.message)
+        }
+    }
+    catch(error){
         res.status(500).send(error.message);
     }
+    res.status(201).send("User created");
+    
 });
 
+app.post("/api/capturePokemon", async (req, res) => {
+  const { ID, NAME, TYPE, LOCATIONS } = req.body;
+  try{
+    await sql.query`INSERT INTO captured_pokemon (TrainerID, PokemonNAME, PokemonTYPE, PokemonLOCATIONS) VALUES (${ID}, ${NAME}, ${TYPE}, ${LOCATIONS})`;
+  }
+  catch(error){
+    res.status(500).send(error.message);
+}
+});
+
+// Old code for creating a table for each user
+//  app.post("/api/createPokeTable", async (req, res) =>{
+//      const {NAME} = req.body;
+//     try{
+//         const userResult = await sql.query`SELECT ID FROM users WHERE NAME = ${NAME}`
+//          if (userResult.recordset.length === 0) {
+//         return res.status(404).json({ error: 'User not found' });}
+        
+//         //const userId = userResult.recordset[0].ID;
+//         const tableName = `[${NAME}_pokemon]`;
+//         await sql.query(`
+//     CREATE TABLE ${tableName} (
+//     id INT IDENTITY(1,1) PRIMARY KEY,
+//     user_id INT,
+//     name VARCHAR(100) NOT NULL,
+//     type VARCHAR(100) NOT NULL,
+//     locations VARCHAR(500) NULL,
+//     FOREIGN KEY (user_id) REFERENCES users(ID)
+//   );
+// `
+// );
+// res.status(201).json({ message: `Table ${tableName} created successfully` });
+// }
+//     catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// })
+ 
 
 app.put("/api/UpdateUserAtId/:id", async (req, res) => {
 	const { id } = req.params;
@@ -81,7 +115,7 @@ app.put("/api/UpdateUserAtId/:id", async (req, res) => {
 			res.status(404).send("User not found");
 		}
 	} catch (err) {
-		res.status(500).send(err.message);
+		 res.status(500).send(err.message);
 	}
 });
 
@@ -99,6 +133,7 @@ app.delete("/api/deleteUser/:id", async (req, res) => {
     }
 });
 
+
 const SECRET_KEY = "veryveryverysecurekey12345"
 app.post("/api/login", async (req, res) => {
   const { USERNAME, PASSWORD } = req.body;
@@ -114,7 +149,7 @@ app.post("/api/login", async (req, res) => {
       return res.status(401).json({ message: "Invalid password" });
     }
     
-	const token = jwt.sign({USERNAME: user.NAME, EMAIL: user.EMAIL }, SECRET_KEY, { expiresIn: '1h' });
+	const token = jwt.sign({USERNAME: user.NAME, EMAIL: user.EMAIL, ID: user.ID }, SECRET_KEY, { expiresIn: '1h' });
 	res.json( {token });
   } catch (err) {
     res.status(500).send(err.message);
